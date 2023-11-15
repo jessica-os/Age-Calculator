91cca35a6b5777990bccd945cb6143a0d70fd49f
Jessica Oliveira <elisa0js@gmail.com>
2023-11-15 14:48:47 -0300

    melhorando js

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,80 @@
 const form = document.querySelector(".form");
 const btn = document.querySelector(".btn-icon");
-
-const inputDay = document.querySelector("#day");
-const labelDay = document.querySelector(".label-day");
-const wrongValueDay = document.querySelector(".wrong-value-day");
-
-const inputMonth = document.querySelector("#month");
-const labelMonth = document.querySelector(".label-month");
-const wrongValueMonth = document.querySelector(".wrong-value-month");
-
-const inputYear = document.querySelector("#year");
-const labelYear = document.querySelector(".label-year");
-const wrongValueYear = document.querySelector(".wrong-value-year");
+let inputs = document.querySelectorAll("input");
+let labels = document.querySelectorAll("label");
+let spansMgsVazios = document.querySelectorAll(".empty-input");
+let wrongValue = document.querySelectorAll(".wrong-value");
+const msg = document.querySelector(".msg-error");
 
 const resultDay = document.querySelector(".days");
 const resultMonth = document.querySelector(".months");
 const resultYear = document.querySelector(".years");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
+});
+
+btn.addEventListener("click", () => {
+  if (validateEmptyInputs(inputs)) return;
+
+  getDates();
+});
+
+const validateEmptyInputs = (inputs) => {
+  let camposVazio = false;
+  inputs.forEach((input, index) => {
+    if (input.value === "") {
+      labels[index].classList.add("error");
+      spansMgsVazios[index].style.display = "block";
+      input.classList.add("error");
+      camposVazio = true;
+    } else {
+      labels[index].classList.remove("error");
+      spansMgsVazios[index].style.display = "none";
+      input.classList.remove("error");
+      validateDay(inputs);
+      validateMonth(inputs);
+      validaYear(inputs);
+    }
   });
-  btn.addEventListener("click", () => {
-    validateDay();
-    validateMonth();
-    validateYear();
-    getDates();
-  });
-  
+  return camposVazio;
+};
+
+const validateDay = (inputs) => {
+  const day = parseInt(inputs[0].value, 10);
+  if (day > 31 || day <= 0) {
+    inputs[0].nextElementSibling.style.display = "none";
+    wrongValue[0].style.display = "block";
+  } else {
+    wrongValue[0].style.display = "none";
+  }
+};
+
+const validateMonth = (inputs) => {
+  const month = parseInt(inputs[1].value, 10);
+
+  if (month > 12 || month <= 0) {
+    inputs[1].nextElementSibling.style.display = "none";
+    wrongValue[1].style.display = "block";
+  } else {
+    wrongValue[1].style.display = "none";
+  }
+};
+
+const validaYear = (inputs) => {
+  const year = parseInt(inputs[2].value, 10);
+  if (year > 2023 || year <= 0) {
+    inputs[2].nextElementSibling.style.display = "none";
+    wrongValue[2].style.display = "block";
+  } else {
+    wrongValue[2].style.display = "none";
+  }
+};
 
 function getDates() {
-  let day = inputDay.value;
-  let month = inputMonth.value - 1;
-  let year = inputYear.value;
+  let day = inputs[0].value;
+  let month = inputs[1].value - 1;
+  let year = inputs[2].value;
 
   let dateOfbirth = new Date(year, month, day);
   let today = new Date();
@@ -42,51 +85,15 @@ function getDates() {
   resultYear.innerHTML = age.getUTCFullYear() - 1970;
   resultDay.innerHTML = age.getUTCDate() - 1;
   resultMonth.innerHTML = age.getUTCMonth();
-}
-
-function validateDay() {
-  if (day.value === "") {
-    inputDay.classList.add("error");
-    labelDay.classList.add("error");
-    inputDay.nextElementSibling.style.display = "block";
-  } else if (day.value > 31) {
-    inputDay.nextElementSibling.style.display = "none";
-    wrongValueDay.style.display = "block";
-  } else {
-    inputDay.classList.remove("error");
-    labelDay.classList.remove("error");
-    inputDay.nextElementSibling.style.display = "none";
-    wrongValueDay.style.display = "none";
+  if (dateOfbirth > today) {
+    resultYear.innerHTML = "--";
+    resultDay.innerHTML = "--";
+    resultMonth.innerHTML = "--";
+    wrongValue[0].style.display = "block";
+    wrongValue[1].style.display = "block";
+    wrongValue[2].style.display = "block";
+    wrongValue[0].innerHTML = "the date must be in the past";
+    wrongValue[1].innerHTML = "the date must be in the past";
+    wrongValue[2].innerHTML = "the date must be in the past";
   }
 }
-function validateMonth() {
-  if (month.value === "") {
-    inputMonth.classList.add("error");
-    labelMonth.classList.add("error");
-    inputMonth.nextElementSibling.style.display = "block";
-  } else if (month.value > 12) {
-    inputMonth.nextElementSibling.style.display = "none";
-    wrongValueMonth.style.display = "block";
-  } else {
-    inputMonth.classList.remove("error");
-    labelMonth.classList.remove("error");
-    inputMonth.nextElementSibling.style.display = "none";
-    wrongValueMonth.style.display = "none";
-  }
-}
-function validateYear() {
-  if (year.value === "") {
-    inputYear.classList.add("error");
-    labelYear.classList.add("error");
-    inputYear.nextElementSibling.style.display = "block";
-  } else if (year.value > 2023) {
-    inputYear.nextElementSibling.style.display = "none";
-    wrongValueYear.style.display = "block";
-  } else {
-    inputYear.classList.remove("error");
-    labelYear.classList.remove("error");
-    inputYear.nextElementSibling.style.display = "none";
-    wrongValueYear.style.display = "none";
-  }
-}
-
